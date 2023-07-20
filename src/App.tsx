7f09fe7ab6485cdcb82879bcc0f95c340e554375
hg185090 <hg185090@ncr.com>
2023-07-20 12:12:08 +0530

    base setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { LandingContainer } from './features/dashboard/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LandingContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
