c31a29f8d6f0c035b8b5eb468cf7dc662ebb821f
Guvvala <hg185090@ncr.com>
2023-07-21 13:21:44 +0530

    menu component and routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { LandingContainer } from './features/dashboard/dashboard';
 import SearchAppBar from './utils/navbar';
+import { RestaurantMenuContainer } from "./features/restaurantMenu/restaurantMenu";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <SearchAppBar />
-        <LandingContainer />
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<LandingContainer />} />
+              <Route path="menu" element={<RestaurantMenuContainer />} />
+          </Routes>
+      </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/features/restaurantMenu/restaurantMenu.tsx
@@ -0,0 +1,69 @@
+import '../../App.css';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+const RestaurantMenu = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+    return (
+        <div className='restaurantMenu'>
+            <h1>KFC</h1>
+            <Box sx={{ maxWidth: { xs: 320, sm: 680 }, margin: 'auto', bgcolor: 'background.paper' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons="auto"
+                    aria-label="scrollable auto tabs example"
+                >
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                    <Tab label="Item Four" />
+                    <Tab label="Item Five" />
+                    <Tab label="Item Six" />
+                    <Tab label="Item Seven" />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    Item One
+                </TabPanel>
+            </Box>
+        </div>
+    );
+};
+
+
+export const RestaurantMenuContainer = (RestaurantMenu);
