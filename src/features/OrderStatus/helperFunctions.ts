9dd65404f97a182c1a17e3e9cad418b98a16e0a9
np185182 <np185182@ncr.com>
2023-07-21 12:52:27 +0530

    cm2

--- src/features/OrderStatus/helperFunctions.ts
+++ src/features/OrderStatus/helperFunctions.ts
@@ -7,4 +7,12 @@ export const getStatus = (status : string) => {
         case 'ready':
             return "Hooray !! your order is Ready" 
     }
+} 
+
+export const getEstimatedTime = (minutes : number , orderTime : string) => {
+    const dateTime = new Date(orderTime);
+    const expectedTime = new Date(dateTime.getTime() + minutes*60000);
+    const currentTime = new Date();
+    const diff = expectedTime.getTime() - currentTime.getTime();
+    return Math.round(diff / 60000);
 }
--- src/features/OrderStatus/index.tsx
+++ src/features/OrderStatus/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react'
-import Slider from '../dashboard/slider'
 import Card from '@mui/material/Card/Card'
 import { ListItem, ListItemText, Typography, styled } from '@mui/material'
-import { getStatus } from './helperFunctions'
+import { getEstimatedTime, getStatus } from './helperFunctions'
 import List from '@mui/material/List';
 
-
-
-const OrderData = {
-    status: 'preparing',
-    time: 5,
-    OrderDetails: [1, 2, 3, 4, 5]
+const dummyOrderDetails1 = {
+    orderID: "123Arwrwy67BC",
+    orderTime: "2023-07-21T14:45:30",
+    orderItems: [
+        {
+            itemName: "Burger",
+            quantity: 2
+        },
+        {
+            itemName: "Coke",
+            quantity: 1
+        },
+    ]
 }
+
+const dummyOrderDetails2 = {
+    orderID: "3143r38wrrwhABC",
+    orderTime: "2023-07-21T14:45:30",
+    orderItems: [
+        {
+            itemName: "Chicken BarBeque Pizza",
+            quantity: 2
+        },
+        {
+            itemName: "Chesse Pizza",
+            quantity: 1
+        },
+    ]
+}
+
+
+const OrdersData = [
+    {
+        status: 'preparing',
+        time: 5,
+        OrderDetails: dummyOrderDetails1
+    },
+    {
+        status: 'placed',
+        time: 10,
+        OrderDetails: dummyOrderDetails2
+    }
+];
+
 
 const MainContainer = styled('div')({
     "color": "white",
@@ -32,15 +67,15 @@ const AddsContainer = styled('div')({
 const CardsContainer = styled('div')({
     "height": "50vh",
     "width": "100%",
-    "display":"flex",
-    "justifyContent":"center"
+    "display": "flex",
+    "justifyContent": "center"
     // "alignItems" : "center",
 })
 
 const CustomCard = styled(Card)({
     "display": "flex",
     "margin": "5px",
-    "width":"25%",
+    "width": "25%",
     "flexDirection": "column",
     "justifyContent": "space-around",
     "alignItems": "center",
@@ -61,40 +96,27 @@ export default function Orderstatus() {
                 Ads
             </AddsContainer>
             <CardsContainer>
-                <CustomCard>
-                    <Typography>Estimated time</Typography>
-                    <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>9 minutes</Typography>
-                    <Typography>{getStatus(OrderData.status)}</Typography>
-                    <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>Order Details</Typography>
-                    <HorizontalLine />
-                    <Typography>Restaurant Name</Typography>
-                    <Typography>Order #order_id</Typography>
-                    <HorizontalLine />
-                    <List sx={{ width: '80%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                        {OrderData.OrderDetails.map((data, index) => {
-                            return (
-                                    <ListItemText primary={`${1} X  Item ${data}`} />
-                            )
-                        })}
-                    </List>
-                </CustomCard>
-                <CustomCard>
-                    <Typography>Estimated time</Typography>
-                    <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>9 minutes</Typography>
-                    <Typography>{getStatus(OrderData.status)}</Typography>
-                    <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>Order Details</Typography>
-                    <HorizontalLine />
-                    <Typography>Restaurant Name</Typography>
-                    <Typography>Order #order_id</Typography>
-                    <HorizontalLine />
-                    <List sx={{ width: '80%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                        {OrderData.OrderDetails.map((data, index) => {
-                            return (
-                                    <ListItemText primary={`${1} X Item ${data}`} />
-                            )
-                        })}
-                    </List>
-                </CustomCard>
+                {OrdersData.map((data, index) => {
+                    return (
+                        <CustomCard>
+                            <Typography>Estimated time</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>{getEstimatedTime(data.time,data.OrderDetails.orderTime)} Minutes</Typography>
+                            <Typography>{getStatus(data.status)}</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>Order Details</Typography>
+                            <HorizontalLine />
+                            <Typography>Restaurant Name</Typography>
+                            <Typography>Order #{data.OrderDetails.orderID}</Typography>
+                            <HorizontalLine />
+                            <List sx={{ width: '80%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                                {data.OrderDetails.orderItems.map((data, index) => {
+                                    return (
+                                        <ListItemText primary={`${data.quantity} X  Item ${data.itemName}`} />
+                                    )
+                                })}
+                            </List>
+                        </CustomCard>
+                    );
+                })}
             </CardsContainer>
         </MainContainer>
     )
