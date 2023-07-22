bda918db6a43ff8ecb9e8cda09cfb9d277b94c39
Ravinder Rao Polasani <ravinder.polasani>
2023-07-22 16:49:14 +0530

    restaurant readings

--- src/features/SearchMenu/index.tsx
+++ src/features/SearchMenu/index.tsx
@@ -1,10 +1,12 @@
 import Tile from './Tile';
+import json from '../dashboard/restaurants.json';
 
 export default function SearchMenu() {
   return (
     <>
-      <Tile />
-      <Tile />
+      {Object.entries(json).map(([key, value]) => (
+        <Tile title={value.title} rating="3.7(5492)" prepTime="42 mins" />
+      ))}
     </>
   );
 }
